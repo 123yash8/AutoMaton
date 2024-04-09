9e58f40c47e610dbbc3db037b2dc3ef3c23e0ba8
Yash Radhakrishna <60549277+123yash8@users.noreply.github.com>
2024-04-09 19:31:49 +0530

    Delete AutoMaton directory

--- AutoMaton/nuclei.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
- nuclei -as -l urls.txt -severity critical,high,low,medium | tee op/nuclei-op/nuclei_op.txt 
--- AutoMaton/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "enter the full path of the installation directory"
-read yrd
-chmod -R 755 $yrd
